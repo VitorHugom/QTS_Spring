31bd89fd963145722f6afce737353cd498a74c47
Vitor Hugo Moreira Souza <vitor.mor2015@gmail.com>
2024-09-01 12:50:16 -0300

    Ultimo commit

--- src/main/java/com/example/qts/qts_schedule/QtsScheduleRepository.java
+++ src/main/java/com/example/qts/qts_schedule/QtsScheduleRepository.java
@@ -4,14 +4,19 @@ import com.example.qts.qts.Qts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface QtsScheduleRepository extends JpaRepository<QtsSchedule, Long> {
     List<QtsSchedule> findByQts(Qts qts);
+
     @Modifying
     @Transactional
     @Query("DELETE FROM QtsSchedule qs WHERE qs.qts.id = :qtsId")
-    void deleteByQtsId(Long qtsId);
+    void deleteByQtsId(@Param("qtsId") Long qtsId);
+
+    @Query("SELECT qs FROM QtsSchedule qs WHERE qs.professor.id = :professorId AND qs.daysOfWeek.id = :diaId")
+    List<QtsSchedule> findByProfessorIdAndDiaId(@Param("professorId") Long professorId, @Param("diaId") Long diaId);
 }
